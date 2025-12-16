d9cb81d578bca545f676fe67c1720804c71a3da6
kuru9696 <kurumi.inao@gmail.com>
2025-12-16 14:24:21 +0900

    Update main.js

--- templates/main.js
+++ templates/main.js
@@ -1,11 +1,8 @@
 let map = L.map('map').setView([35.70059963531073, 139.49998981936298], 16); 
-/*
-L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png', {
-  attribution: "<a href='https://maps.gsi.go.jp/development/ichiran.html' target='_blank'>地理院タイル</a>"
-}).addTo(map);
-*/
+
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
 let apikey = "b5c83b29-0210-4ffd-98d1-006e854bbc2f"
+
